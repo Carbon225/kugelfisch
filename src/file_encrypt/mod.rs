dda3882f226db7715256ff0844a4ec19aae2ebd9
Carbon <carbon.szlachta@gmail.com>
2021-01-26 17:08:21 +0100

    New file processing

--- src/file_encrypt/mod.rs
+++ src/file_encrypt/mod.rs
@@ -10,25 +10,17 @@ fn algo(x: u64) -> u64 {
 }
 
 fn process_file(cbc: &mut dyn BlockProcessor<u64>,
-                ifile: &mut dyn Read,
-                ofile: &mut dyn Write) -> Result<(), Box<dyn Error>> {
-    let mut block = 0;
-    let mut pad = 0;
-    for (i, b) in ifile.bytes().enumerate() {
-        let b = b?;
-        block <<= 8;
-        block += b as u64;
-        if i % 8 == 7 {
-            let block = cbc.next(block);
-            ofile.write(&block.to_be_bytes())?;
+                input: &mut dyn Read,
+                output: &mut dyn Write) -> Result<(), Box<dyn Error>> {
+    loop {
+        let mut buf = [0; 8];
+        if input.read(&mut buf)? <= 0 {
+            break;
         }
-        pad = 7 - (i % 8);
-    }
 
-    if pad > 0 {
-        block <<= pad * 8;
+        let block = u64::from_le_bytes(buf);
         let block = cbc.next(block);
-        ofile.write(&block.to_be_bytes())?;
+        output.write(&block.to_le_bytes())?;
     }
 
     Ok(())
@@ -38,7 +30,7 @@ pub fn encrypt_file(input: &mut dyn Read,
                     output: &mut dyn Write) -> Result<(), Box<dyn Error>> {
     // TODO generate IV
     let iv: u64 = 0x6162616261626162;
-    output.write_all(&iv.to_be_bytes())?;
+    output.write_all(&iv.to_le_bytes())?;
 
     let mut encryptor = Encryptor::new(&algo, iv);
     process_file(&mut encryptor, input, output)?;
@@ -48,11 +40,9 @@ pub fn encrypt_file(input: &mut dyn Read,
 
 pub fn decrypt_file(input: &mut dyn Read,
                     output: &mut dyn Write) -> Result<(), Box<dyn Error>> {
-    let mut iv: [u8; 8] = [0; 8];
-    for (i, byte) in input.bytes().take(8).enumerate() {
-        iv[i] = byte?;
-    }
-    let iv = u64::from_be_bytes(iv);
+    let mut iv = [0; 8];
+    input.read_exact(&mut iv)?;
+    let iv = u64::from_le_bytes(iv);
 
     let mut decryptor = Decryptor::new(&algo, iv);
     process_file(&mut decryptor, input, output)?;
