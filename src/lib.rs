6049175efe35fb273d1943575a4dbfdad770b6ab
Carbon <carbon.szlachta@gmail.com>
2021-01-29 02:03:20 +0100

    new arg parsing

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,13 @@ use std::fmt::{Display, Debug, Formatter};
 
 pub struct ProgramConfig<'a> {
     input_path: &'a str,
+    output_path: &'a str,
     passphrase: Option<&'a str>,
 }
 
 impl<'a> ProgramConfig<'a> {
-    pub fn new(input_path: &'a str, passphrase: Option<&'a str>) -> Self {
-        ProgramConfig { input_path, passphrase }
+    pub fn new(input_path: &'a str, output_path: &'a str, passphrase: Option<&'a str>) -> Self {
+        ProgramConfig { input_path, output_path, passphrase }
     }
 }
 
@@ -27,7 +28,7 @@ pub enum ProgramIntent<'a> {
 
 #[derive(Debug)]
 pub enum ProgramError {
-    InvalidMode(String),
+    InvalidArgs(String),
     ErrorOpening(String, Box<dyn Error>),
     ErrorCreating(String, Box<dyn Error>),
     OperationFailed(Box<dyn Error>),
@@ -36,7 +37,7 @@ pub enum ProgramError {
 impl Display for ProgramError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", match &self {
-            ProgramError::InvalidMode(mode) => format!("Invalid mode '{}'", mode),
+            ProgramError::InvalidArgs(msg) => msg.clone(),
             ProgramError::ErrorOpening(file, cause) => format!("Error opening '{}': {}", file, cause),
             ProgramError::ErrorCreating(file, cause) => format!("Error creating '{}': {}", file, cause),
             ProgramError::OperationFailed(cause) => format!("Operation failed: {}", cause),
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,56 @@
 use kugelfisch::{ProgramIntent, ProgramConfig, ProgramError, run_program};
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
+fn print_usage(arg0: &str) {
+    eprintln!("Usage: {} [-e] [-d] [-p PASSPHRASE] INPUT OUTPUT", arg0);
+}
+
+fn parse_args(args: &Vec<String>) -> Result<ProgramIntent, ProgramError> {
+    let err = |msg: &str| Err(ProgramError::InvalidArgs(msg.to_owned()));
+    let contains = |x: &str| args.iter().any(|a| a == x);
+
     if args.len() < 3 {
-        eprintln!("Usage: {} e|d FILE [PASSPHRASE]", args[0]);
-        return;
+        return err("Not enough arguments");
     }
 
-    let passphrase = if args.len() < 4 {
-        None
-    } else {
-        Some(args[3].as_str())
-    };
+    if !contains("-d") && !contains("-e") {
+        return err("Mode not given");
+    }
 
-    let config = ProgramConfig::new(&args[2], passphrase);
-    match args[1].as_str() {
-        "e" => run_program(&ProgramIntent::Encrypt(config)),
-        "d" => run_program(&ProgramIntent::Decrypt(config)),
-        _ => Err(ProgramError::InvalidMode(String::from(&args[1])))
-    }.unwrap_or_else(|e| eprintln!("{}", e));
+    if contains("-d") && contains("-e") {
+        return err("Conflicting arguments: -d -e");
+    }
+
+    let pass = args.windows(2)
+        .find_map(|p| if p[0] == "-p" {
+            Some(&p[1])
+        } else {
+            None
+        });
+
+    if args.iter().rev().take(2).any(|arg|
+        arg == "-e" || arg == "-d" || (pass.is_some() && arg == pass.unwrap())) {
+        return err("Missing input/output");
+    }
+
+    let config = ProgramConfig::new(
+        &args[args.len() - 2],
+        &args[args.len() - 1],
+        pass.map(|p| p.as_str()));
+
+    if contains("-d") {
+        Ok(ProgramIntent::Decrypt(config))
+    } else {
+        Ok(ProgramIntent::Encrypt(config))
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    match parse_args(&args) {
+        Ok(intent) => run_program(&intent),
+        Err(e) => Err(e),
+    }.unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        print_usage(&args[0]);
+    });
 }
