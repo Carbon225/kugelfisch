4acc787fc1e39f3632c78b8a5724d9906955cb4c
Carbon <carbon.szlachta@gmail.com>
2021-01-31 14:04:56 +0100

    error exit code

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let args_ref: Vec<&str> = args.iter().map(|arg| arg.as_str()).collect();
 
-    if let Some(e) = parse_args(args_ref).and_then(|intent| run_program(&intent)).err() {
+    if let Err(e) = parse_args(args_ref).and_then(|intent| run_program(&intent)) {
         eprintln!("{}", e);
         if let ProgramError::InvalidArgs(_) = e {
             eprintln!("Usage: {} [-e] [-d] [-p PASSPHRASE] INPUT OUTPUT", args[0]);
         }
+        std::process::exit(1);
     }
 }
